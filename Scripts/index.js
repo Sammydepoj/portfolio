8660c8c2588463be9ca2c9ae9368524d7c8bb39c
Sammydepoj <samueladepoju86@yahoo.com>
2022-09-28 16:23:41 +0100

    dark mode added

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,33 +3,49 @@ let showMenu =document.getElementById('ul');
 let menu = document.getElementsByClassName('menu-icon')[0];
 
 
-function aboutshow() {
-    if (show.style.display="none") {
-        show.style.display="block"
-    }
-    else{
-        show.style.display="none";
-    }
+
+
+
+function modeToggle(){
+    var element = document.body;
+  element.classList.toggle("dark-mode") 
 }
 
+// var brandName =document.getElementById("brandName");
+
+// if (element.classList !='dark-mode') {
+
+//     brandName.style.color='#25282B';
+// }
+//     else{
+//         brandName.style.color='white';
+//     }
+    
+
+
+
+
+
+function aboutshow() {
+    if (show.style.display=="block") {
+        show.style.display="none"
+    }
+    else{
+        show.style.display="block";
+    }
+}
 function aboutClose(){
     show.style.display="none"
 }
-
 // harmbuger
     menu.onclick=function(){
     document.getElementById('ul').style.display="block";
 }
-
-
-
-
 function closeModal(){
-    if (showMenu.style.display="block") {
+    if (showMenu.style.display=="block") {
         showMenu.style.display="none"
     }
     else{
         showMenu.style.display="block";
     }
-
 }
